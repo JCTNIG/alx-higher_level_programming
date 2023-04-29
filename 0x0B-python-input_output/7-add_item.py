218210f3c630fd4c56c685b535b163569b582b13
JCtech <ocjustc@gmail.com>
2023-04-29 14:43:59 +0100

    7. Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,14 +2,17 @@
 
 """ This module contains fnc that adds all args to a list """
 
-import sys
+mport sys
+
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-    
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
     try:
-        my_obj = load_from_json_file("add_item.json")
+        items = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        my_obj = []
-        my_obj.extend(sys.argv[1:])
-        save_to_json_file(my_obj, 'add_item.json')
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
+
